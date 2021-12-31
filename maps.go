9d0f26dce968675067adfa5a92a6370b060b738b
dogan <dogan@DESKTOP-COQSKES>
2021-12-31 22:56:07 +0300

    Initial commit

---
+++ maps.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		map c#taki dictionary gibidir iki şekilde tanımlanır.
+	*/
+	//1
+	var myMap map[string]int = make(map[string]int)
+	myMap["first"]=1
+	myMap["second"]=2
+	//2
+	secondMap := make(map[string]int)
+	secondMap["1"]=11
+	fmt.Println(myMap, secondMap)
+
+	/*
+		virgülden sonraki değişken ile o key'e ait value var mı kontrolü yapılır
+		eğer key varsa ok değişkeni true alır ve key değeri myValue'ya atanır.
+		eğer keye karşı bir değer yoksa myValue int default olarak 0 alır
+		eğer myValue değeri kullanımına ihtiyaç yok ise _ ile değiştirilir
+		eğer ok değişkeni kullanılmıyorsa unused hatası döner. bu durum if koşuluyla kontrol edilir
+	*/
+	//_,ok := myMap["first5"]
+	//myValue,ok := myMap["first5"]
+	//fmt.Println(ok, myValue)
+	//alttaki ornek: ok true ise dogru atama yapılmıstır ve terminale output verir
+	if myValue,ok := myMap["first"]; ok==true{
+		fmt.Println( myValue)
+	}
+	//3
+	thirdMap := map[string]int {
+		"first":11,
+		"second":22,
+	}
+	fmt.Println(thirdMap)
+	//mapten item silme
+	delete(thirdMap,"second")
+	fmt.Println(thirdMap)
+}
