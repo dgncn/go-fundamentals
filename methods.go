272239244d486fe6fbbdd36241779659f6224515
dogan <dogan@DESKTOP-COQSKES>
2022-01-06 21:13:11 +0300

    methods updated

--- methods.go
+++ methods.go
@@ -8,10 +8,15 @@ import (
 type carInterface interface {
 	accelerate()
 	decelerate()
+	increaseSpeed(speed int) *car
+	getSpeedValue() int
+	get()
 }
 
 //carInterface'ini uygulayan tip tanımı
-type car struct{}
+type car struct {
+	_speed int
+}
 
 //method tanımı. car değişkeni tarafından kullanılacak bir method
 func (myCar car) accelerate() {
@@ -22,10 +27,26 @@ func (myCar car) decelerate() {
 	fmt.Println("car is decelerating")
 }
 
+//car nesnesinin referansı üstünde field set yapar ve aldığı nesneyi döner
+func (myCar *car) increaseSpeed(speedVal int) *car {
+	(*myCar)._speed = speedVal
+	return myCar
+}
+
+func (myCar *car) getSpeedValue() int {
+	return (*myCar)._speed
+}
+
+func (myCar *car) get() {
+	fmt.Println(myCar)
+}
+
 func main() {
 	var carInt carInterface
-	carInt = car{}
+	carInt = &car{}
 	carInt.accelerate()
 	carInt.decelerate()
-
+	carInt.increaseSpeed(150)
+	fmt.Println(carInt.getSpeedValue())
+	carInt.get()
 }
